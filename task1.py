4adbf3bff0be07e62b409fde5a29ac9adc21aaac
Tortisemilk420 <159003787+Tortisemilk420@users.noreply.github.com>
2024-06-05 13:31:35 -0700

    4g

--- task1.py
+++ task1.py
@@ -7,3 +7,42 @@ Create entry widets to allow user to enter their:
 
 Create a button so that when they click on the button, it states all of the information in a 4th entry widget
 """
+import tkinter as tk 
+
+def run(e):
+   
+    print("This does not show up on your gui")
+    print(f"The details of the event are {e}")
+    data1 = e2.get()
+    data2 = e3.get()
+    data3 = e4.get()
+    Totaldata = " Name :" + data1+ " Student number :" + data2 + " Grade :" + data3 
+    e1.delete(0,tk.END)
+    e1.insert(0,Totaldata)
+   
+    
+   
+
+win = tk.Tk()
+e1 = tk.Entry(win,width=75)
+b1 = tk.Button(win, text="Click to change the text")
+e2 = tk.Entry(win,width=15)
+l1 = tk.Label(win, width=15, text="Name")
+e3 = tk.Entry(win,width=15)
+l2 = tk.Label(win, width=15, text="student number")
+e4 = tk.Entry(win,width=15)
+l3 = tk.Label(win, width=15, text="grade")
+b1.bind("<Button-1>",run)
+
+l1.pack()
+e2.pack()
+l2.pack()
+e3.pack()
+l3.pack()
+e4.pack()
+b1.pack()
+e1.pack()
+
+
+
+win.mainloop()
--- task2.py
+++ task2.py
@@ -6,3 +6,39 @@ The first 2 entry widgets allow the user to enter in the 2 short sides of a righ
 When the button is clicked, calculate the length of the hypotenuse and display it in the 3rd entry widget.
 Any labels you need for instruction are optional.
 """
+import tkinter as tk 
+win = tk.Tk()
+
+eoutput = tk.StringVar()
+eoutput.set("Output goes here")
+
+def clickFunction(event):
+    print(event)
+    data1 = e2.get()
+    data2 = e3.get()
+    data1 = float(data1)
+    data2 = float(data2)
+    Totaldata = ((data1**2) + (data2**2))**0.5
+    e1.delete(0,tk.END)
+    e1.insert(0,Totaldata)
+    b1.bind("<Button>",clickFunction)
+
+win = tk.Tk()
+e1 = tk.Entry(win,width=15)
+b1 = tk.Button(win, text="Click to get hyp")
+e2 = tk.Entry(win,width=15)
+l1 = tk.Label(win, width=15, text="Number1")
+e3 = tk.Entry(win,width=15)
+l2 = tk.Label(win, width=15, text="Number2")
+
+
+l1.pack()
+e2.pack()
+l2.pack()
+e3.pack()
+b1.pack()
+e1.pack()
+
+
+
+win.mainloop()
